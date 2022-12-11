18f0853e806e2ee032b077ddf6675c23affc969f
Bruno Cabral <cabral.brc@gmail.com>
2022-12-11 00:42:03 -0800

    split logistic

--- main/LogisticRegression.cpp
+++ main/LogisticRegression.cpp
@@ -14,10 +14,17 @@ int main(int argc, char *argv[])
     int cols = dataset[0].size();
 
     Eigen::MatrixXd dataMat = etl.CSVtoEigen(dataset, rows, cols);
-    
+
     Eigen::MatrixXd norm = etl.Normalize(dataMat, false);
 
-    std::cout << norm.topRows(10) << std::endl;
+    Eigen::MatrixXd X_train, y_train, X_test, y_test;
+    std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd> split_data = etl.TrainTestSplit(norm, 0.8);
+    std::tie(X_train, y_train, X_test, y_test) = split_data;
+
+    std::cout << "X_train shape(" << X_train.rows() << "," << X_train.cols() << ")" << std::endl;
+    std::cout << "y_train shape(" << y_train.rows() << "," << y_train.cols() << ")" << std::endl;
+    std::cout << "X_test shape(" << X_test.rows() << "," << X_test.cols() << ")" << std::endl;
+    std::cout << "y_test shape(" << y_test.rows() << "," << y_test.cols() << ")" << std::endl;
 
     return EXIT_SUCCESS;
 }
